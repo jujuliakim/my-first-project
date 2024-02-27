c46c0b0acd850e62d3f3e8ad9d3ddd011b6f1c73
jujuliakim <julietta.km@gmail.com>
2024-02-27 12:26:28 +0600

    todo_site

--- todo_site/todo_site/urls.py
+++ todo_site/todo_site/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mysite project.
+URL configuration for todo_site project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,9 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
+from todo import views
+ 
 urlpatterns = [
+    #####################home_page###########################################
+    path('', views.index, name="todo"),
+    ####################give id no. item_id name or item_id=i.id ############
+    # pass item_id as primary key to remove that the todo with given id
+    path('del/<str:item_id>', views.remove, name="del"),
+    ########################################################################
     path('admin/', admin.site.urls),
-    path('', include('blog.urls'))
 ]
